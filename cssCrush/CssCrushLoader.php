b83faa44cc37ea7913f5840abc780290d9c80f11
rwollenburg <roman@slash-works.de>
2014-09-19 15:11:56 +0200

    static method "recompile" added for global reuse

--- cssCrush/CssCrushLoader.php
+++ cssCrush/CssCrushLoader.php
@@ -1,35 +1,43 @@
 <?php
-/**
- * cssCrush for Contao Open Source CMS
- *
- * Copyright (C) 2013 Joe Ray Gregory
- *
- * @package cssCrush
- * @link    http://borowiakziehe.de
- * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
- */
+    /**
+     * cssCrush for Contao Open Source CMS
+     *
+     * Copyright (C) 2013 Joe Ray Gregory
+     *
+     * @package cssCrush
+     * @link    http://borowiakziehe.de
+     * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+     */
 
-namespace slashworks;
+    namespace slashworks;
+
+    ini_set("display_errors","1");
+
+    class CssCrushLoader extends \Frontend
+    {
 
 
-class CssCrushLoader extends \Frontend
-{
+        static $oCssCrushFile;
+        static $aOptions;
+        static $cssCrushCtoCombiner;
     /**
      * Hook into generatePage and add csscrush to the output head
-     * @param $objPage
-     * @param $objLayout
+     *
+     * @param              $objPage
+     * @param              $objLayout
      * @param \PageRegular $objPageRegular
+     *
      * @return string
      */
     public function loadCSSCrush($objPage, $objLayout, \PageRegular $objPageRegular)
     {
-        if($objLayout->useCssCrush == 'cssCrush') {
+        if ($objLayout->useCssCrush == 'cssCrush') {
 
             //Get CSS File and check if file exists
-            $objFile = \FilesModel::findByPk($objLayout->cssCrushFile);
+            CssCrushLoader::$oCssCrushFile = \FilesModel::findByPk($objLayout->cssCrushFile);
+            CssCrushLoader::$cssCrushCtoCombiner = $objLayout->cssCrushCtoCombiner;
 
-            if ($objFile === null || !is_file(TL_ROOT . '/' . $objFile->path))
-            {
+            if (CssCrushLoader::$oCssCrushFile === null || !is_file(TL_ROOT . '/' . CssCrushLoader::$oCssCrushFile->path)) {
                 return '';
             }
 
@@ -40,44 +48,68 @@ class CssCrushLoader extends \Frontend
             $plugins = self::generatePluginArrays($objLayout);
 
             //generate options array. Look at for all possible csscrush options: https://github.com/peteboere/css-crush/wiki/PHP-API
-            $options = array(
-                'minify' => ($objLayout->cssCrushMinify) ?  true :  false,
-                'cache' => ($objLayout->cssCrushCache) ?  true :  false,
-                'versioning' => ($objLayout->cssCrushVersioning) ?  true :  false,
-                'output_dir' => ($objLayout->cssCrushDirName) ?  TL_ROOT.'/'.$objLayout->cssCrushDirName :  false,
-                'output_file' => ($objLayout->cssCrushFileName) ?  $objLayout->cssCrushFileName :  false,
-                'context' => ($objLayout->cssCrushContext) ?  $objLayout->cssCrushContext :  false,
-                'source_map' => ($objLayout->cssCrushSourceMap) ?  true :  false,
-                'enable' => $plugins['enabled'],
-                'disable' => $plugins['disabled']
+            CssCrushLoader::$aOptions = array(
+                'minify'      => ($objLayout->cssCrushMinify) ? true : false,
+                'cache'       => ($objLayout->cssCrushCache) ? true : false,
+                'versioning'  => ($objLayout->cssCrushVersioning) ? true : false,
+                'output_dir'  => ($objLayout->cssCrushDirName) ? TL_ROOT . '/' . $objLayout->cssCrushDirName : false,
+                'output_file' => ($objLayout->cssCrushFileName) ? $objLayout->cssCrushFileName : false,
+                'context'     => ($objLayout->cssCrushContext) ? $objLayout->cssCrushContext : false,
+                'source_map'  => ($objLayout->cssCrushSourceMap) ? true : false,
+                'enable'      => $plugins['enabled'],
+                'disable'     => $plugins['disabled']
             );
 
-            if($objLayout->cssCrushDocRoot) {
+
+            if ($objLayout->cssCrushDocRoot) {
                 $options['doc_root'] = $objLayout->cssCrushDocRoot;
             }
 
             // generate css
-            $compiled_file = csscrush_file(TL_ROOT.'/'.$objFile->path, $options);
-
-            // if combine option is set
-            if($objLayout->cssCrushCtoCombiner)
-                $strStatic = "||static";
-
-            //add file to template
-            $GLOBALS['TL_CSS'][] = $compiled_file .$strStatic;
+            CssCrushLoader::recompile(CssCrushLoader::$aOptions);
         }
+    }
+
+    /**
+     * (re)compile css file with given css crush options
+     *
+     * @param $sPath
+     * @param $aOptions
+     *
+     * @return string
+     */
+    public static function recompile($aOptions)
+    {
+        // Load cssCrush compiler
+        require_once 'system/modules/cssCrush/vendor/cssCrush/CssCrush.php';
+        $strStatic = "";
+        // set filepath
+        $sPath = TL_ROOT . '/' . CssCrushLoader::$oCssCrushFile->path;
+        //merge given options with defaultoptions
+        CssCrushLoader::$aOptions = array_merge(CssCrushLoader::$aOptions, $aOptions);
+        //crush css file
+        $compiled_file = csscrush_file($sPath, CssCrushLoader::$aOptions);
+
+        // if combine option is set
+        if (CssCrushLoader::$cssCrushCtoCombiner) {
+            $strStatic = "||static";
+        }
+
+        //add file to template
+        $GLOBALS['TL_CSS'][CssCrushLoader::$oCssCrushFile->hash] = $compiled_file . $strStatic;
     }
 
 
     /**
      * Get all ccscrush plugins from plugins folder (vendor/cssCrush/plugins) and return them as aray
+     *
      * @return array
      */
     static public function getPlugins()
     {
         $data = array();
 
-        if ($handle = opendir(TL_ROOT .'/'. 'system/modules/cssCrush/vendor/cssCrush/plugins')) {
+        if ($handle = opendir(TL_ROOT . '/' . 'system/modules/cssCrush/vendor/cssCrush/plugins')) {
 
             while (false !== ($file = readdir($handle))) {
                 if ($file != "." && $file != "..") {
@@ -87,27 +119,31 @@ class CssCrushLoader extends \Frontend
             closedir($handle);
 
         }
+
         return $data;
     }
 
 
     /**
      * Generate the enable disable plugins and return them as array
+     *
      * @param $objLayout
+     *
      * @return array
      */
-    private function generatePluginArrays($objLayout) {
+    private function generatePluginArrays($objLayout)
+    {
         $activePlugins = deserialize($objLayout->cssCrushPlugins);
 
         $allPlugins = self::getPlugins();
-        if(is_array($activePlugins)){
-            $disabledPlugins = array_diff($allPlugins,$activePlugins);
+        if (is_array($activePlugins)) {
+            $disabledPlugins = array_diff($allPlugins, $activePlugins);
         }
 
 
         return array
         (
-            'enabled' => $activePlugins,
+            'enabled'  => $activePlugins,
             'disabled' => $disabledPlugins
         );
     }
