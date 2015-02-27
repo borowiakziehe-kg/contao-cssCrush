ab60e681115eaeb062cb9766761598099716ce4d
Stefan Melz <info@stefanmelz.de>
2015-02-27 11:20:03 +0100

    Update CssCrushLoader.php
    Damit wird es am Ende des Heads geladen.

--- cssCrush/CssCrushLoader.php
+++ cssCrush/CssCrushLoader.php
@@ -94,7 +94,9 @@
         }
 
         //add file to template
-        $GLOBALS['TL_CSS'][CssCrushLoader::$oCssCrushFile->hash] = $compiled_file . $strStatic;
+        //$GLOBALS['TL_CSS'][CssCrushLoader::$oCssCrushFile->hash] = $compiled_file . $strStatic;
+        $GLOBALS['TL_HEAD'][] = "<link rel='stylesheet' href='$compiled_file.$strStatic'/>";
+
     }
 
 
